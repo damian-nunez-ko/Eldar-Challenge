da717bd0b306a2d4edc5164f8c2a429c39ad330e
Damian Nuñez Kohler <dnunezko@itba.edu.ar>
2022-08-31 14:31:06 -0300

    added default method getrate to cardissuer enum

--- src/eldar/creditcard/models/CardIssuer.java
+++ src/eldar/creditcard/models/CardIssuer.java
@@ -4,24 +4,27 @@ import java.time.LocalDate;
 
 public enum CardIssuer {
     VISA {
+        @Override
         public double getRate() {
             LocalDate today = LocalDate.now();
             double rate = ((double) today.getYear())/((double) today.getMonthValue());
-            return validateRate(rate);
+            return validateRate(rate) / 100;
         }
     },
     NARA {
+        @Override
         public double getRate() {
             LocalDate today = LocalDate.now();
             double rate = today.getDayOfMonth() * NARA_RATE_MULTIPLIER;
-            return validateRate(rate);
+            return validateRate(rate) / 100;
         }
     },
     AMEX {
+        @Override
         public double getRate() {
             LocalDate today = LocalDate.now();
             double rate = today.getMonthValue() * AMEX_RATE_MULTIPLIER;
-            return validateRate(rate);
+            return validateRate(rate) / 100;
         }
     };
 
@@ -30,6 +33,10 @@ public enum CardIssuer {
 
     private static final double NARA_RATE_MULTIPLIER = 0.5;
     private static final double AMEX_RATE_MULTIPLIER = 0.1;
+
+    public double getRate() {
+        return 0.0;
+    }
 
     public double validateRate(double rate) {
         if(rate < MIN_RATE) {
